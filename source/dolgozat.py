e3cd534c6ea4dc24b1119de151d03db073d095e6
Antal Kormányos <44323201+swisskanton@users.noreply.github.com>
2024-10-21 20:24:02 +0200

    Create dolgozat.py

---
+++ source/dolgozat.py
@@ -0,0 +1,82 @@
+# Hozd létre a name változót és a felhasználótól kérd be az értékét
+# 2 pont
+
+
+# Írasd ki a következő üzenetet a neme változó felhasználásával
+# "Hello name felhasználó!"
+# 2 pont
+
+
+# Írasd ki 5-ször egymás mellé a name változót szólözzel elválasztva!
+# 2 pont
+
+
+# Kérj be egy egész és egy tizedes számot!
+# 2 pont
+
+
+# Írd ki a két szám szorzatát!
+# 2 pont
+
+
+# Kérj be egy hatványt, és írasd ki a tizedes szám egész hatványát!
+# 2 pont
+
+
+# Döntsd el, hogy az egész szám páros vagy páratlan!
+# 2 pont
+
+
+# Döntsd el, hogy melyik szám a nagyobb!
+# 2 pont
+
+
+# Készíts véletlen számot a két szám között!
+# 2 pont
+
+
+# Kerekítsd egészre a tizedes számot!
+# 2 pont
+
+
+# Írd ki a kerekített szám szorzótábláját 1-től 10-ig!
+# 2 pont
+
+
+# Add össze a lista elemeit (két megoldás)!
+num_list = [1, 9, 1, 6, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 2]
+# 1. megoldás
+# 2 pont
+
+
+# 2. megoldás ciklussal
+# 2 pont
+
+
+# Találd meg a legkisebb és a legnagyobb elemet (két megoldás)!
+# 1. megoldás
+# 2 pont
+
+
+# 2. megoldás ciklussal
+# 2 pont
+
+
+# Rendezd a lista másolatát növekvő sorrendbe!
+# 2 pont
+
+
+# Írasd ki a rendezett lista középső 6 elemét!
+# 2 pont
+
+
+# Készíts a lista nem egyedi elemeiből egy tuple-t!
+# 2 pont
+
+
+# Készíts a listából egy set-et!
+# 2 pont
+
+
+# Készíts egy listát, amely kételemű tuple-t tartalmaz (elem, darabszám)!
+# 2 pont
